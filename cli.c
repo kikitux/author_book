3f5a877e15becbb1e275aefcc900c9b88f1bc6cc
Alvaro Miranda <kikitux@gmail.com>
2015-12-28 19:37:14 +1300

    update printf

--- cli.c
+++ cli.c
@@ -13,19 +13,20 @@ int main() {
     struct word temp_title;
     struct word temp_topic;
 
-    strcpy(temp_fn.word, "alvaro");
-    strcpy(temp_ln.word, "miranda");
+    strcpy(temp_fn.word, "Alvaro");
+    strcpy(temp_ln.word, "Miranda");
 
     author1 = assign_author(&temp_fn, &temp_ln);
 
     strcpy(temp_title.word, "y u no devops");
     long int temp_isbn = 123123123;
 
-    strcpy(temp_topic.word, "devops");
+    strcpy(temp_topic.word, "DevOps");
 
     book1 = assign_book(author1, &temp_title, temp_isbn, &temp_topic);
 
-    printf("Hello from cli %s %s\n", author1.firstname->word,author1.lastname->word);
+    printf("Hello from cli %s\n", author1.firstname->word);
     printf("Author of %s, category: %s\n", book1.title->word, book1.topic->word);
+    printf("Want to quote this book? use:\nISBN %ld - %s - %s\n", book1.isbn, book1.title->word, author1.lastname->word);
     return 0;
 }
